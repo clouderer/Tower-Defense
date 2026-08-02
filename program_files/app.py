f815c6614eb0ec21753f0dbe2873b87575b68027
mrdka <le35509116@gmail.com>
2026-08-02 23:01:09 +0200

    Main Update

--- program_files/app.py
+++ program_files/app.py
@@ -1,16 +1,106 @@
 import pygame
 
-from screen.types.main_menu     import MainMenu
-from screen.types.map_selection import MapSelection
-from screen.types.gameplay      import Gameplay
-from screen.types.settings_menu import SettingsMenu
-from screen.types.pause_menu    import PauseMenu
-from screen.types.result_screen import ResultScreen
+from .screen.types.gameplay      import Gameplay
+from .maps.game_map              import GameMap
+from pathlib                     import Path
 
-
-from maps.game_map              import GameMap
 DISPLAY_WIDTH = 600 
 DISPLAY_HEIGHT = 350
+
+
+tmp_path = [
+    (1,176),
+    (26,176),
+    (26,76),
+    (126,76),
+    (126,176),
+    (76,176),
+    (76,276),
+    (226,276),
+    (226,176),
+    (176,176),
+    (176,76),
+    (326,76),
+    (326, 176),
+    (276, 176),
+    (276, 276),
+    (426, 276),
+    (426, 176),
+    (376, 176),
+    (376, 76),
+    (526, 76),
+    (526, 201)
+]
+
+tmp_tower_positions = [
+    (51,101), (76,101),
+    (51,126), (76,126),
+
+    (126,26),
+    (151,26),
+
+    (151,301),
+    (176,301),
+
+    (101,201),
+    (126,201),
+    (151,201),
+    (176,201),
+
+    (101,226),
+    (126,226),
+    (151,226),
+    (176,226),
+
+    (201,101),
+    (226,101),
+    (251,101),
+    (276,101),
+
+    (201,126),
+    (226,126),
+    (251,126),
+    (276,126),
+
+    (301,26),
+    (326,26),
+    (351,26),
+    (376,26),
+    (401,26),
+
+    (301,201),
+    (326,201),
+    (351,201),
+    (376,201),
+
+    (301,226),
+    (326,226),
+    (351,226),
+    (376,226),
+
+    (401,101),
+    (426,101),
+    (451,101),
+    (476,101),
+
+    (401,126),
+    (426,126),
+    (451,126),
+    (476,126),
+
+    (451, 151),
+    (476, 151),
+
+    (451, 176),
+
+    (376,301),
+    (401,301),
+
+]
+
+tmp_image_path = Path(__file__).resolve().parents[1]/"asset_files"/"maps"/"neon_grid.png"
+tmp_image = pygame.image.load(tmp_image_path)
+tmp_game_map = GameMap(tmp_image, tmp_path, tmp_tower_positions)
 
 class App: 
     def __init__(self): 
@@ -20,7 +110,7 @@ class App:
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.current_screen = Gameplay(self) # [TO DO]
+        self.current_screen = Gameplay(self, tmp_game_map) # [TO DO]
 
     #____METHODS____ 
 
@@ -43,8 +133,6 @@ class App:
             pygame.display.update() 
 
         pygame.quit() 
-
-tmp_game_map = GameMap()
 
 game = App()
 game.run()
