00fa00a8efe17d953cbc02b51f649b16e59d4b57
mrdka <le35509116@gmail.com>
2026-09-08 00:06:43 +0200

    Reformatting to standard PIP8

--- program_files/screen/screen.py
+++ program_files/screen/screen.py
@@ -5,9 +5,10 @@ App ──current_screen──> MainMenu    App controls the screen
 App <──────app────────  Screen      Screen requests action from app
 """
 
-class Screen(ABC): 
-    def __init__(self, app): 
-        self.app = app 
+
+class Screen(ABC):
+    def __init__(self, app):
+        self.app = app
 
     @abstractmethod
     def handle_event(self, event):
